6b92ba1fc0808ea739c965eec7e42ee1f5ef193f
srijansood <srijansood@gmail.com>
2015-04-20 22:25:14 -0400

    cleanup

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
 var pg = require('pg');
-var express = require('express');
 var stormpath = require('express-stormpath');
-var app = express();
-//var homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
-//var keyfile = homedir + '/Dropbox/Studies/CS2340/web/.stormpath/apiKey.properties';
 var pwd = process.cwd();
 var keyfile = pwd + '/.stormpath/apiKey.properties';
 
@@ -26,20 +22,12 @@ app.use(express.static(__dirname + '/public'));
 app.use(stormpathMiddleware);
 
 app.get('/', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i = 0; i < times; i++) {
-        result += cool();
-    }
-    //uses the env variable TIMES from .env to append the result TIMES times.
-
     response.render('home', {
     title: 'Welcome'
     });
-
-    response.send(result);
 });
 
+//Databse using PostgreSQL
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
