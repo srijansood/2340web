c1eb175d49c45886dd56976e442534b622169cbb
srijansood <srijansood@gmail.com>
2015-04-20 22:13:28 -0400

    User Profile

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ app.get('/db', function (request, response) {
   });
 })
 
+app.use('/profile',stormpath.loginRequired,require('./profile')());
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
