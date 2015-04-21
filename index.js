f6f05784ca1a8ed11ba5e07c61070374ee927ced
srijansood <srijansood@gmail.com>
2015-04-21 02:25:45 -0400

    fb login test

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var express = require('express');
-var app = express();
 var cool = require('cool-ascii-faces');
 var pg = require('pg');
 var stormpath = require('express-stormpath');
 var pwd = process.cwd();
 var keyfile = pwd + '/.stormpath/apiKey.properties';
+var fbSecret = pwd + '/.facebook.properties';
+var app = express();
 
 var stormpathMiddleware = stormpath.init(app, {
   apiKeyFile: keyfile,
   application: 'https://api.stormpath.com/v1/applications/68wtNUaBNZUM3HicQ3PEQr',
   secretKey: 'some_long_random_string',
   expandCustomData: true,
-  enableForgotPassword: true
+  enableForgotPassword: true,
+  enableFacebook: true,
+  social: {
+    facebook: {
+      appId: '729604067154235',
+      appSecret: 'a3fc4cae48579ba686e12df1a56800f9',
+      }
+    }
 });
-
 
 app.set('views', './views');
 app.set('view engine', 'jade');
@@ -21,7 +28,10 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(stormpathMiddleware);
 
+
 app.get('/', function(request, response) {
+
+    console.log('LOGFBSECRET!!!!!!-' + fbSecret);
     response.render('home', {
     title: 'Welcome'
     });
